3cbe03ad7027ead7d2c7a1e935836ed6eccb7a26
Subhrajeet-Biswal <subhrajeet.biswal@bakerhughes.com>
2024-03-13 10:35:03 +0530

    some colour changes

--- Backend/src/db/connection.ts
+++ Backend/src/db/connection.ts
@@ -19,20 +19,4 @@ async function connectdb(){
   }
 }
 
-// function connectdb() {
-//   const sequelize = new Sequelize("Mitigation-db", "postgres", process.env.DB_PASSWORD, {
-//     host: "localhost",
-//     dialect: "postgres",
-//   });
-
-//   sequelize
-//     .authenticate()
-//     .then(() => {
-//       console.log("Connection has been established successfully.");
-//     })
-//     .catch((error) => {
-//       console.error("Unable to connect to the database: ", error);
-//     });
-// }
-
 export {connectdb,client};
